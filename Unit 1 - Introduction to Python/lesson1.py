6efb50c8493514935daeae4f991cf80e14641485
Blaqcodex <117004858+Blaqcodex@users.noreply.github.com>
2026-07-15 12:13:49 +0200

    Add initial lesson1.py file with basic print and input functionality

---
+++ Unit 1 - Introduction to Python/lesson1.py
@@ -0,0 +1,13 @@
+# Making the computer speak
+
+print("Hello, World!")
+
+# Printing user name
+
+name = "Thabiso"
+print(name)
+
+# Making our code interactive
+
+user_name = input("What is your name? ")
+print(f"Greetings, {user_name}!")
