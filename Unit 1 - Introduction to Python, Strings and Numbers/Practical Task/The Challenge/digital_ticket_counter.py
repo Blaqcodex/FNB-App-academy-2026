4cf730364ef7d1152bdbd24a2afd2a57e6801593
Blaqcodex <117004858+Blaqcodex@users.noreply.github.com>
2026-07-21 22:55:32 +0200

    Add digital ticket counter program to confirm bookings

---
+++ Unit 1 - Introduction to Python, Strings and Numbers/Practical Task/The Challenge/digital_ticket_counter.py
@@ -0,0 +1,8 @@
+# This program is a digital ticket counter that takes the user's name and the band/artist they want to see, and then confirms the booking.
+
+name = input("What is your name? ")
+
+artist = input("Which band/artist would you like to see? ")
+
+print(f"Hey {name}! Your tickets to see {artist} are booked successfully!")
+
